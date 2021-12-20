a78059bc0035b3910bfe362dacb4a0a7dd178e58
bryantakari <bryantakari2018@gmail.com>
2021-12-20 18:22:24 +0700

    mvcpackage

--- src/Program.java
+++ src/Program.java
@@ -2,12 +2,13 @@
 public class Program {
 
 	public Program() {
+		System.out.println("Test");
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Program();
 	}
 
 }
