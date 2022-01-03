6fbe554b199b8e5dce592dc485f5548f79f8ea85
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2022-01-03 22:56:49 +0700

    Update controller & model layer of voucher

--- src/Program.java
+++ src/Program.java
@@ -16,16 +16,72 @@ public class Program {
 //		AuthHandler.getInstance().viewLoginForm();
 //		CartHandler.getInstance().viewCartManagementForm();
 //		Product product = new Product();
-		Product product = new Product();
-		
-		CartHandler.getInstance().getCart().add(new CartItem(10,product.getProduct(1), 2));
-		CartHandler.getInstance().getCart().add(new CartItem(2, product.getProduct(2), 1));
-		CartHandler.getInstance().getCart().add(new CartItem(3,product.getProduct(3), 3));
-		Employee employee = new Employee();
-		AuthHandler.getInstance().setEmployee(employee.getEmployee("username1"));
-		ProductHandler.getInstance().viewProductManagementForm();
+//		Product product = new Product();
+//		
+//		CartHandler.getInstance().getCart().add(new CartItem(10,product.getProduct(1), 2));
+//		CartHandler.getInstance().getCart().add(new CartItem(2, product.getProduct(2), 1));
+//		CartHandler.getInstance().getCart().add(new CartItem(3,product.getProduct(3), 3));
+//		Employee employee = new Employee();
+//		AuthHandler.getInstance().setEmployee(employee.getEmployee("username1"));
+//		ProductHandler.getInstance().viewProductManagementForm();
 //		VoucherHandler.getInstance().viewVoucherManagementForm();
-		
+
+		System.out.println("TEST");
+		System.out.println("======================");
+		System.out.println("AuthHandler");
+		AuthHandler authHandler = AuthHandler.getInstance();
+		System.out.println("+login(String username, String password): Boolean");
+		Boolean isLoggedin = authHandler.login("username1", "password1");
+		System.out.println(isLoggedin);
+		System.out.println(authHandler.getStatusMessage());
+		System.out.println("======================");
+		System.out.println("CartHandler");
+		CartHandler cartHandler = CartHandler.getInstance();
+		System.out.println("+addToCart(productID, quantity) : CartItem");
+		cartHandler.addToCart("1", "1");
+		System.out.println(cartHandler.getStatusMessage());
+		cartHandler.addToCart("2", "-1");
+		System.out.println(cartHandler.getStatusMessage());
+		System.out.println("+getCart: List<CartItem>");
+		cartHandler.getCart().parallelStream().forEach(x -> System.out.println(x));
+		System.out.println(cartHandler.getStatusMessage());
+		System.out.println("======================");
+		System.out.println("EmployeeHandler");
+		Employee employee = new Employee();
+		EmployeeHandler employeeHandler = EmployeeHandler.getInstance();
+		System.out.println("+insertEmployee(name, position, salary, username, password) : employee");
+		employee = employeeHandler.insertEmployee("namet", "barista", 1000, "usernamet", "passwordt");
+		System.out.println(employeeHandler.getStatusMessage());
+		System.out.println(employee);
+		System.out.println("+getEmployee(username) : employee");
+		employee = employeeHandler.getEmployee("usernamet");
+		System.out.println(employee);
+		System.out.println(employeeHandler.getStatusMessage());
+		System.out.println("+updateEmployee(employeeID, name, position, salary, username, password) : employee");
+		employee = employeeHandler.updateEmployee(employee.getEmployeeID(), "namett", "barista", 1001, "usernamett",
+				"passwordtt");
+		System.out.println(employee);
+		System.out.println(employeeHandler.getStatusMessage());
+		System.out.println("+deleteEmployee(employeeID) : boolean");
+		Boolean isEmployeeDeleted = employeeHandler.deleteEmployee(employee.getEmployeeID());
+		System.out.println(isEmployeeDeleted);
+		System.out.println(employeeHandler.getStatusMessage());
+		System.out.println("+getAllEmployees() : list<employee>");
+		employeeHandler.getAllEmployees().parallelStream().forEach(x -> System.out.println(x));
+		System.out.println(employeeHandler.getStatusMessage());
+		System.out.println("======================");
+		System.out.println("PositionHandler");
+		Position position = new Position();
+		PositionHandler positionHandler = PositionHandler.getInstance();
+		System.out.println("+getAllPositions() : list<position>");
+		positionHandler.getAllPositions().parallelStream().forEach(x -> System.out.println(x));
+		System.out.println(positionHandler.getStatusMessage());
+		System.out.println("+getPosition(positionID) : position");
+		position = positionHandler.getPosition(1);
+		System.out.println(position);
+		System.out.println(positionHandler.getStatusMessage());
+		System.out.println("");
+
 	}
 
 	public static void main(String[] args) {
